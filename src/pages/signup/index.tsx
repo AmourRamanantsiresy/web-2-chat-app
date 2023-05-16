cef2c6899dd13255aae2f70d5bdb26595708b269
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-05-16 10:30:21 +0300

    feat: confirm password validator

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -28,25 +28,20 @@ const SignInPage = () => {
         <div className='flex justify-center items-center w-screen h-screen bg-white'>
           <div className='flex flex-col mx-2 w-screen rounded-2xl border-none item-center md:w-1/2 lg:w-2/5 2xl:1/4'>
             <h1 className='m-8 text-5xl text-center text-black'>SignUp</h1>
-            <form
-              className='self-center px-8 pb-10 rounded'
-              onSubmit={handleSubmit}
-            >
+            <form className='self-center px-8 pb-10 rounded' onSubmit={handleSubmit}>
               <div className='mt-12 mb-1'>
                 <Input label='name' name='name' />
                 <Input label='Email' name='email' />
-                <Input label='Password' name='password' />
+                <Input type='password' label='Password' name='password' />
                 <Input
+                  type='password'
                   label='Confirm password'
                   name='confirmPassword'
                   validate={(pass: string) => pass.length > 10 || 'Not enough'}
                 />
               </div>
               <div className='flex justify-end w-full'>
-                <button
-                  type='submit'
-                  className='px-6 py-2 text-white bg-blue-400 rounded hover:bg-blue-500'
-                >
+                <button type='submit' className='px-6 py-2 text-white bg-blue-400 rounded hover:bg-blue-500'>
                   Send
                 </button>
               </div>
--- src/pages/signup/utils.ts
+++ src/pages/signup/utils.ts
@@ -1,11 +1,17 @@
 import { CreateUser } from '@/common/types';
 import zod from 'zod';
 
-export const signUpSchema = zod.object({
-  name: zod.string().min(5).max(15),
-  email: zod.string().email(),
-  password: zod.string().min(8),
-});
+export const signUpSchema = zod
+  .object({
+    name: zod.string().min(5).max(60),
+    email: zod.string().email(),
+    password: zod.string().min(8),
+    confirmPassword: zod.string(),
+  })
+  .refine(({ confirmPassword, password }) => password === confirmPassword, {
+    message: "Password doesn't match.",
+    path: ['confirmPassword'],
+  });
 
 export type SignUpSchema = zod.infer<typeof signUpSchema>;
 
